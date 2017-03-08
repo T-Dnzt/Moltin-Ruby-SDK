7f0130674dc154d6600ec0f80f9053ef9d800637
T-Dnzt <thi.denizet@gmail.com>
2017-03-08 13:03:21 +0700

    Fix Rubocop offenses in examples/

--- examples/init.rb
+++ examples/init.rb
@@ -3,11 +3,9 @@ require 'moltin'
 Moltin.configure do |config|
   config.client_id = 'YOUR_CLIENT_ID'
   config.client_secret = 'YOUR_CLIENT_SECRET'
-  config.base_url  = 'https://api.yourdomain.com'
+  config.base_url = 'https://api.yourdomain.com'
 end
 
-Moltin::Client.new({
-  client_id: 'YOUR_CLIENT_ID',
-  client_secret: 'YOUR_CLIENT_SECRET',
-  base_url:  'https://api.yourdomain.com'
-})
+Moltin::Client.new(client_id: 'YOUR_CLIENT_ID',
+                   client_secret: 'YOUR_CLIENT_SECRET',
+                   base_url:  'https://api.yourdomain.com')
